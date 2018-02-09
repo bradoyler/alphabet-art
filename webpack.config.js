0638d06b7731dc31729e0a6b13f832647266b763
Brad Oyler <bradoyler@gmail.com>
2018-02-09 08:24:35 -0500

    fix publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: 'dist/',
     filename: 'build.js'
   },
   module: {
